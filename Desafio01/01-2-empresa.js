5b085c37348ffefab63501e0cf39596980db6a18
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-02-03 21:34:55 -0300

    Construção e impressão de vetores e objetos

---
+++ Desafio01/01-2-empresa.js
@@ -0,0 +1,16 @@
+// Construção e impressão de objetos
+
+const usuario = {
+    nome: "Gustavo",
+    empresa: {
+        nome: "Rocketseat",
+        cor:  "Roxo",
+        foco: "Programação",
+        endereco: {
+            rua: "Rua Guilherme Gembala",
+            numero: 260
+        }
+    }
+}
+
+console.log(`A empresa ${usuario.empresa.nome} está localizada em ${usuario.empresa.endereco.rua}, ${usuario.empresa.endereco.numero}`)
